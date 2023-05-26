a5c66e4ba5212017ab6abe80d17f8ec964e171c3
Muhammed Eren Altun <muhammederen.altun@karatas.web.tr>
2023-05-26 17:51:33 +0300

    İndex Sayfası düzenlendi navbar menü üste alındı index sayfasının içi doldurulmalı.

--- Data/SupportService.cs
+++ Data/SupportService.cs
@@ -22,5 +22,35 @@ namespace CompanyRecProject.Data
             await _dataContext.SaveChangesAsync();
             return true;
         }
+        // id lere göre kaydedilen ftaleplerin kaydını getirme
+        public async Task<Support> GetSupportsById(int id)
+        {
+            Support support = await _dataContext.Supports.FirstOrDefaultAsync(x => x.SupportId == id);
+            return support;
+
+        }
+        // talep bilgisi düzenle
+        public async Task<bool> UpdateSupportDetails(Support support)
+        {
+            _dataContext.Supports.Update(support);
+            await _dataContext.SaveChangesAsync(true);
+            return true;
+        }
+        public async Task<bool> UpdateSupportStatus(Support support)
+        {
+            var newStatus = "İşlemde";
+            support.SupportStatus = (string)newStatus;
+
+            return true;
+        }
+
+        // talep sil
+        public async Task<bool> DeleteSupports(Support support)
+        {
+            _dataContext.Supports.Remove(support);
+            await _dataContext.SaveChangesAsync(true);
+            return true;
+        }
     }
 }
+
---
+++ Migrations/20230522075359_Supports.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompanyRecProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class Supports : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SupportStatus",
+                table: "Supports",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ResentTransactions",
+                table: "Supports",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Requester",
+                table: "Supports",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Request",
+                table: "Supports",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmployeeName",
+                table: "Supports",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SupportStatus",
+                table: "Supports",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ResentTransactions",
+                table: "Supports",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Requester",
+                table: "Supports",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Request",
+                table: "Supports",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmployeeName",
+                table: "Supports",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
